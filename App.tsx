18a8ab78f4ff7456c8b8067438a12bdae95fb42b
Nik <nerdvestor@gmail.com>
2021-07-08 12:25:03 -0700

    born

--- App.tsx
+++ App.tsx
@@ -1,24 +1,27 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import * as eva from "@eva-design/eva"
+import { ApplicationProvider } from "@ui-kitten/components"
+import { StatusBar } from "expo-status-bar"
+import React from "react"
+import "react-native-gesture-handler"
+import { SafeAreaProvider } from "react-native-safe-area-context"
+import useCachedResources from "./hooks/useCachedResources"
+import useColorScheme from "./hooks/useColorScheme"
+import Navigation from "./navigation"
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+	const isLoadingComplete = useCachedResources()
+	const colorScheme = useColorScheme()
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+	if (!isLoadingComplete) {
+		return null
+	} else {
+		return (
+			<SafeAreaProvider>
+				<ApplicationProvider {...eva} theme={eva.light}>
+					<Navigation colorScheme={colorScheme} />
+					<StatusBar />
+				</ApplicationProvider>
+			</SafeAreaProvider>
+		)
+	}
 }
